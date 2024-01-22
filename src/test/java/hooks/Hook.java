8094a0ab6855097be07775565a79a098553dc07b
Garima Khare <Garima.Khare@Geminisolutions.com>
2024-01-22 16:37:19 +0530

    Added required files for template

---
+++ src/test/java/hooks/Hook.java
@@ -0,0 +1,13 @@
+package hooks;
+
+import com.gemini.generic.exception.GemException;
+import com.gemini.generic.ui.utils.DriverManager;
+import io.cucumber.java.Before;
+
+public class Hook {
+    @Before
+    public static void hook() throws GemException {
+        DriverManager.setUpBrowser();
+
+    }
+}
